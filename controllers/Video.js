6ef6fe490781e50564bbb6883bcb899133dc9025
Ibad Ahmad <ibad.ahmad@euphoria.pk>
2022-05-13 15:47:06 +0500

    add file mp4

--- controllers/Video.js
+++ controllers/Video.js
@@ -3,18 +3,18 @@
 const router = require("express")();
 const fs = require("fs");
 
-router.get("/", function (req, res) {
+router.get("/:name", function (req, res) {
     const range = req.headers.range;
-    const video = req.headers.video;
+    const video = req.params.name;
 
     if (!range || !video) {
         res.status(400).send("Requires Range/Video Title");
     }
 
-    const videoPath = __dirname + "/video.mp4";
+    const videoPath = __dirname + `/../assets/${video}`;
     const videoSize = fs.statSync(videoPath).size;
 
-    const CHUNK_SIZE = 10 ** 7; // 10 MB
+    const CHUNK_SIZE = 10 ** 6; // 1 MB
     const start = Number(range.replace(/\D/g, ""));
     const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
